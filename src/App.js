f544b014c60a8e40ca0a6bce75a08ebc2ace7ced
Joel <j2joelgeorgejohn@gmail.com>
2023-07-08 13:54:54 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Gallery from "./pages/gallery"
@@ -12,26 +12,71 @@ import "./App.css"
 function App() {
 
   const [activeItem, setActiveItem] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const imgs = [
+      "./assets/back.jpg",
+      "./assets/scrollimages/1a.jpg",
+      "./assets/scrollimages/2a.jpg",
+      "./assets/scrollimages/3a.jpg",
+      "./assets/scrollimages/4.jpg",
+      "./assets/scrollimages/5.jpg",
+      "./assets/scrollimages/6.jpg",
+      "./assets/scrollimages/7.jpg",
+      "./assets/scrollimages/8.jpg",
+      "./assets/scrollimages/9.jpg",
+      "./assets/scrollimages/10.jpg",
+      "./assets/scrollimages/11.jpg",
+      "./assets/scrollimages/12.jpg",
+      "./assets/scrollimages/13.jpg",
+    ];
+
+    cacheImages(imgs);
+  }, []);
+
+  const cacheImages = async (srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image();
+
+        img.src = src;
+        img.onload = resolve();
+        img.onerror = reject();
+      });
+    });
+
+    await Promise.all(promises);
+
+    setIsLoading(false);
+  }
 
   return (
     <ActiveContext.Provider value={{activeItem, setActiveItem}}>
       <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={ 
-            <Home />} />
-          <Route exact path="/home" element={ 
-            <Home />} />
-          <Route path="/gallery" element={ 
-            <Gallery />} />
-          <Route path="/chapters" element={ 
-              <Chapters />} />
-          <Route path="/about" element={
-              <AboutUs />} />
-          <Route path="/events" element={
-              <EventPart />} />
-        </Routes>
-      </BrowserRouter>
+        {
+          isLoading ?
+            <div className='spinner'>
+              Spinning
+            </div>
+            :
+            <BrowserRouter>
+              <Routes>
+                <Route index element={ 
+                  <Home />} />
+                <Route exact path="/home" element={ 
+                  <Home />} />
+                <Route path="/gallery" element={ 
+                  <Gallery />} />
+                <Route path="/chapters" element={ 
+                    <Chapters />} />
+                <Route path="/about" element={
+                    <AboutUs />} />
+                <Route path="/events" element={
+                    <EventPart />} />
+              </Routes>
+            </BrowserRouter>
+        }
       </div>
       </ActiveContext.Provider>
   );
